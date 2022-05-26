e02aef8b9789ffd38785ccae95affdbe15683d67
Rafael.Schaker <Rafael.Schaker01@edu.pucrs.br>
2022-05-26 10:48:48 -0300

    adicionando tabela artistas

--- trabalho.sql
+++ trabalho.sql
@@ -3,5 +3,14 @@ CREATE TABLE Pessoas(
 	nome	VARCHAR2(100)	NOT NULL,
 	email 	VARCHAR2(100)	NOT NULL,
 	CONSTRAINT pk_pessoas  	PRIMARY KEY (cod_ID),
-	CONSTRAINT uk_cursos_cod_ID UNIQUE (cod_ID),
+	CONSTRAINT uk_pessoas_cod_ID UNIQUE (cod_ID)
+);
+
+CREATE TABLE Artistas(
+    nome_art    VARCHAR2(100)   NOT NULL,
+    estudio     VARCHAR2(100)   NOT NULL,
+    cod_ID      NUMBER(5)       NOT NULL,
+    CONSTRAINT pk_artistas  PRIMARY KEY (nome_art),
+    CONSTRAINT fk_pes_art   FOREIGN KEY (cod_ID),
+    CONSTRAINT uk_artistas_nome_art UNIQUE (nome_art)
 );
