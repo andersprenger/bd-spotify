a09609f847f3c0184d5374c674a091ef7460fbba
Rafael-Schaker <71292308+Rafael-Schaker@users.noreply.github.com>
2022-06-16 16:41:49 -0300

    Create musica.sql

---
+++ musica.sql
@@ -0,0 +1,9 @@
+INSERT INTO Musica  (nome,musica_ID,nome_art,duracao,qnt_estrelas,estilo_musical) 
+Values              ('Mad with you','Ashnikko',224,4.5,'pop alternativo'),
+                    ('Do céu a terra','Torpse, Luan Santana',162,3,'pagode'),
+                    ('Bom te ver','Ashnikko',120,2.5,'rock'),
+                    ('Cadeirante levado','Torpse',320,0,'pop'),
+                    ('Oriana','Ashnikko, Torpse',90,5,'pop alternativo'),
+                    ('Gosto roxo','Luan Santana',186,1,'pagode'),
+                    ('Mad with you','Torpse',224,3.75,'reggie'),
+                    ('Bom te ver','Luan Santana',224,4.8,'rock'),
